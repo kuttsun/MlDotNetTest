7e654af722ad51bb7be21bb7f71fe6b20fe100fc
kuttsun <tsukushimail+github@gmail.com>
2019-05-31 15:48:19 +0900

    チュートリアルのソースを追加

---
+++ TaxiFarePrediction/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using Microsoft.ML;
+
+namespace TaxiFarePrediction
+{
+    class Program
+    {
+        static readonly string _trainDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "taxi-fare-train.csv");
+        static readonly string _testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "taxi-fare-test.csv");
+        static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "Model.zip");
+
+        static void Main(string[] args)
+        {
+            MLContext mlContext = new MLContext(seed: 0);
+
+            var model = Train(mlContext, _trainDataPath);
+
+            Evaluate(mlContext, model);
+
+            TestSinglePrediction(mlContext, model);
+        }
+
+        /// <summary>
+        /// モデルをトレーニングする
+        /// </summary>
+        /// <returns>トレーニング済みモデル</returns>
+        public static ITransformer Train(MLContext mlContext, string dataPath)
+        {
+            // テキストファイルの読み込み
+            IDataView dataView = mlContext.Data.LoadFromTextFile<TaxiTrip>(dataPath, hasHeader: true, separatorChar: ',');
+
+            var pipeline = mlContext.Transforms.CopyColumns(outputColumnName: "Label", inputColumnName: "FareAmount")
+                .Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "VendorIdEncoded", inputColumnName: "VendorId"))
+                .Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "RateCodeEncoded", inputColumnName: "RateCode"))
+                .Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "PaymentTypeEncoded", inputColumnName: "PaymentType"))
+                .Append(mlContext.Transforms.Concatenate("Features", "VendorIdEncoded", "RateCodeEncoded", "PassengerCount", "TripTime", "TripDistance", "PaymentTypeEncoded"))
+                // データ変換定義に FastTreeRegressionTrainer 機械学習タスクを追加
+                .Append(mlContext.Regression.Trainers.FastTree());
+
+            // モデルをトレーニング dataview に適合させ、トレーニング済みモデルを返します。
+            var model = pipeline.Fit(dataView);
+            return model;
+        }
+
+        /// <summary>
+        /// モデルを評価する
+        /// </summary>
+        private static void Evaluate(MLContext mlContext, ITransformer model)
+        {
+            // テスト データセットを読み込みます
+            IDataView dataView = mlContext.Data.LoadFromTextFile<TaxiTrip>(_testDataPath, hasHeader: true, separatorChar: ',');
+            // Test データを変換します
+            var predictions = model.Transform(dataView);
+
+            var metrics = mlContext.Regression.Evaluate(predictions, "Label", "Score");
+
+            Console.WriteLine();
+            Console.WriteLine($"*************************************************");
+            Console.WriteLine($"*       Model quality metrics evaluation         ");
+            Console.WriteLine($"*------------------------------------------------");
+
+            Console.WriteLine($"*       RSquared Score:      {metrics.RSquared:0.##}");
+
+            Console.WriteLine($"*       Root Mean Squared Error:      {metrics.RootMeanSquaredError:#.##}");
+        }
+
+        /// <summary>
+        /// モデルを使用して予測を行う
+        /// </summary>
+        private static void TestSinglePrediction(MLContext mlContext, ITransformer model)
+        {
+            // 運賃を予測する
+            var predictionFunction = mlContext.Model.CreatePredictionEngine<TaxiTrip, TaxiTripFarePrediction>(model);
+
+            var taxiTripSample = new TaxiTrip()
+            {
+                VendorId = "VTS",
+                RateCode = "1",
+                PassengerCount = 1,
+                TripTime = 1140,
+                TripDistance = 3.75f,
+                PaymentType = "CRD",
+                FareAmount = 0 // To predict. Actual/Observed = 15.5
+            };
+
+            var prediction = predictionFunction.Predict(taxiTripSample);
+
+            Console.WriteLine($"**********************************************************************");
+            Console.WriteLine($"Predicted fare: {prediction.FareAmount:0.####}, actual fare: 15.5");
+            Console.WriteLine($"**********************************************************************");
+        }
+    }
+}
---
+++ TaxiFarePrediction/TaxiTrip.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.ML.Data;
+
+namespace TaxiFarePrediction
+{
+    /// <summary>
+    /// 入力データクラス
+    /// </summary>
+    public class TaxiTrip
+    {
+        [LoadColumn(0)]
+        public string VendorId;
+
+        [LoadColumn(1)]
+        public string RateCode;
+
+        [LoadColumn(2)]
+        public float PassengerCount;
+
+        [LoadColumn(3)]
+        public float TripTime;
+
+        [LoadColumn(4)]
+        public float TripDistance;
+
+        [LoadColumn(5)]
+        public string PaymentType;
+
+        [LoadColumn(6)]
+        public float FareAmount;
+    }
+
+    /// <summary>
+    /// 予測結果
+    /// </summary>
+    public class TaxiTripFarePrediction
+    {
+        [ColumnName("Score")]
+        public float FareAmount;
+    }
+}
